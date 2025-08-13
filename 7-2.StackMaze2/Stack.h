2ec1fc29d44432c223a335e62ce95cf33fbaf020
dovewith999 <limhs991013@gmail.com>
2025-08-13 14:56:39 +0900

    NEW : Stack을 이용한 DFS 구현

---
+++ 7-2.StackMaze2/Stack.h
@@ -0,0 +1,86 @@
+﻿#pragma once
+#include <iostream>
+#include <Windows.h>
+
+#define FORCEINLINE __forceinline
+
+// 최대 저장 수
+constexpr int MAX_STACK_COUNT = 100;
+
+template <typename T>
+class Stack
+{
+public:
+	explicit Stack() = default;
+	~Stack() = default;
+
+public:
+	// 정리 함수
+	void Clear()
+	{
+		memset(datas, 0, sizeof(datas));
+		count = 0;
+	}
+
+	// 스택이 비었는지 확인
+	FORCEINLINE bool IsEmpty() const { return count == 0; }
+
+	// 스택이 가득 찼는지 확인
+	FORCEINLINE bool IsFull() const { return count == MAX_STACK_COUNT; }
+
+	// 스택에 데이터 추가
+	bool Push(const T& data)
+	{
+		if (IsFull())
+		{
+			//MessageBoxA();
+			std::cout << "스택이 가득차있어 추가가 불가능합니다.\n";
+			return false;
+		}
+
+		// 전달된 경험치를 데이터에 저장
+		datas[count] = data;
+		++count;
+		return false;
+	}
+
+	bool Push(T&& data)
+	{
+		if (IsFull())
+		{
+			std::cout << "스택이 가득차있어 추가가 불가능합니다.\n";
+			return false;
+		}
+
+		// 전달된 경험치를 데이터에 저장
+		datas[count] = std::move(data);
+		++count;
+		return false;
+	}
+
+	// 스택에서 데이터 추출
+	bool Pop(T& outValue) 
+	{
+		if (IsEmpty())
+		{
+			std::cout << "스택이 비어있어 추가가 불가능합니다.\n";
+			return false;
+		}
+
+		//--count;
+
+		outValue = datas[--count];
+		return true;
+	}
+
+public:
+	FORCEINLINE int Count() const { return count; }
+
+private:
+	// 데이터 저장 공간
+	T datas[MAX_STACK_COUNT];
+
+	// 스택에 저장된 자료 수
+	// = 데이터를 추가할 위치의 인덱스
+	int count = 0;
+};
---
+++ 7.StackMaze/main.cpp
@@ -0,0 +1,122 @@
+﻿#include "Stack.h"
+
+//2차원 좌표를 나타내는 클래스
+class Vector2
+{
+public:
+	explicit Vector2() = default;
+	explicit Vector2(int x, int y)
+		: x(x), y(y)
+	{
+	}
+	~Vector2() = default;
+
+public:
+	//출력 편의를 위해 연산자 오버로딩
+	friend std::ostream& operator<<(std::ostream os, const Vector2 position)
+	{
+		return os << "(" << position.x << ", " << position.y << ")";
+	}
+
+public:
+	int x = 0;
+	int y = 0;
+};
+
+// 작은 맵.
+constexpr size_t MAZE_SIZE = 6;
+// map[y][x];
+// s: 시작지점, g: 목표지점, 1: 벽, 0: 길
+char map[MAZE_SIZE][MAZE_SIZE] =
+{
+	{'1','1','1','1','1','1'},
+	{'s','0','1','0','0','1'},
+	{'1','0','0','0','1','1'},
+	{'1','0','1','0','1','1'},
+	{'1','0','1','0','0','g'},
+	{'1','1','1','1','1','1'}
+};
+
+// 가려는 위치가 이동 가능한지 확인하는 함수
+// 입력은 행(y), 열(x)순서로
+bool IsValidLocation(int y, int x)
+{
+	if (y >= MAZE_SIZE || x >= MAZE_SIZE || y < 0 || x < 0)
+	{
+		return false;
+	}
+
+	//확인
+	return map[y][x] == '0' || map[y][x] == 'g';
+}
+
+int main()
+{
+	// 시작위치 검색
+	int startX = 0;
+	int startY = 0;
+
+	for (int y = 0; y < MAZE_SIZE; ++y)
+	{
+		for (int x = 0; x < MAZE_SIZE; ++x)
+		{
+			std::cout << map[y][x] << " ";
+
+			if (map[y][x] == 's')
+			{
+				startX = x;
+				startY = y;
+			}
+		}
+
+		std::cout << '\n';
+	}
+
+	// 시작 위치부터 탐색
+	// 스택에 삽입
+	Stack<Vector2> locationStack;
+	locationStack.Push(Vector2(startX, startY));
+
+	while (!locationStack.IsEmpty())
+	{
+		// 현재 위치 반환
+		Vector2 currentLocation;
+		locationStack.Pop(currentLocation);
+		std::cout << &currentLocation << " ";
+
+		if (map[currentLocation.y][currentLocation.x] == 'g')
+		{
+			std::cout << "\n미로탐색종료\n";
+			std::cin.get();
+			return 0;
+		}
+
+		map[currentLocation.y][currentLocation.x] = '.';
+
+		// 앞으로 이동할 위치를 stack에 삽입
+		// 상
+		if (IsValidLocation(currentLocation.y - 1, currentLocation.x))
+		{
+			locationStack.Push(Vector2(currentLocation.x, currentLocation.y - 1));
+		}
+		// 하 
+		if (IsValidLocation(currentLocation.y + 1, currentLocation.x))
+		{
+			locationStack.Push(Vector2(currentLocation.x, currentLocation.y + 1));
+		}
+		//좌
+		if (IsValidLocation(currentLocation.y, currentLocation.x - 1))
+		{
+			locationStack.Push(Vector2(currentLocation.x - 1, currentLocation.y));
+		}
+		//우
+		if (IsValidLocation(currentLocation.y, currentLocation.x + 1))
+		{
+			locationStack.Push(Vector2(currentLocation.x + 1, currentLocation.y));
+		}
+	}
+
+	std::cin.get();
+
+	return 0;
+}
